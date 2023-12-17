ddf0e1c92cacb9d4216c5dc89c95e4f7f0729f68
hidenShark <ilushashark@gmail.com>
2023-12-17 14:01:32 +0400

    Initial Commit

---
+++ gutax/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gutax
+//
+//  Created by Илья Акулов on 17.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
